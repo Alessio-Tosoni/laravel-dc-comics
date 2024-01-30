5b60b3929f7252846b68f63ce6e455ca5c6988b7
Alessio-Tosoni <alessiotosoni988@gmail.com>
2024-01-30 18:01:14 +0100

    completa prima milestone e bonus seeder

---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $array_comics = config("comics");
+
+        foreach($array_comics as $comics_item){
+            $nuovo_comic = new Comic();
+            $nuovo_comic->title = $comics_item["title"];
+            $nuovo_comic->price = $comics_item["price"];
+            $nuovo_comic->cover = $comics_item["thumb"];
+            $nuovo_comic->save();
+        }
+    }
+}
